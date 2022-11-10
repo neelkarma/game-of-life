b3e58f9fd7aca71e7d2a9a1c13b962b8b28b7971
chickensalt <nsdj.sharma@gmail.com>
2022-11-10 20:29:13 +1100

    document code better

--- src/main.rs
+++ src/main.rs
@@ -8,11 +8,13 @@ async fn main() {
     let mut grid = Grid::random(screen_width() as usize, screen_height() as usize);
     let mut dimensions = (screen_width(), screen_height());
     loop {
+        // Reset grid if window was resized
         if (screen_width(), screen_height()) != dimensions {
             grid = Grid::random(screen_width() as usize, screen_height() as usize);
             dimensions = (screen_width(), screen_height());
         }
 
+        // Render grid
         for y in 0..grid.height {
             for x in 0..grid.width {
                 if grid.get_cell(x, y) {
@@ -22,6 +24,8 @@ async fn main() {
                 }
             }
         }
+
+        // Update grid and await next frame
         grid.update();
         next_frame().await;
     }
